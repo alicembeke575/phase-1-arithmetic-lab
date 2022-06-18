f3ba418857201de7414ed67b51b1ed3c259d923e
Alice Mukulu <106523765+alicembeke575@users.noreply.github.com>
2022-06-18 04:09:59 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
-// Write your code here
+var num1 = 62;
+var num2 = 1;
+let multiply = num1 * num2
+multiply;
+let random = Mth.random() + 1
+random = 1.4685907922792847
+var num3 =49;
+var num4 = 9;
+let mod = num3 % num4;
+mod = 4
+var max = Math.max(5,10,15,20);
+max = 20
